f7951833d885b3e6659256bcbdd5148c7a6bbf91
Lenny <phtttttt@gmail.com>
2017-06-13 22:21:20 -0400

    fixed some mw stats stuff

--- src/main/java/eladkay/hudpixel/command/statsgamemodes/MegaWallsStats.kt
+++ src/main/java/eladkay/hudpixel/command/statsgamemodes/MegaWallsStats.kt
@@ -32,15 +32,27 @@ object MegaWallsStats {
         outText.append(McColorHelper.GOLD + "Coins: " + McColorHelper.BOLD + megawallsStats.get("coins"))
         outText.append("\n")
         outText.append("\n")
-        // TODO add final kills and stuff.
+
+        // Regular kills.
         outText.append(McColorHelper.GOLD + "Kills: " + McColorHelper.BOLD + megawallsStats.get("kills") + McColorHelper.RESET + McColorHelper.GOLD +
                 " | Deaths: " + McColorHelper.BOLD + megawallsStats.get("deaths") + McColorHelper.RESET + McColorHelper.GOLD +
                 " | K/D " + McColorHelper.BOLD + StatsCommand.calculateKD(megawallsStats.get("kills").asInt, megawallsStats.get("deaths").asInt))
         outText.append("\n")
+        outText.append("\n")
+
+        // Final kills.
+        outText.append(McColorHelper.GOLD + "Final Kills: " + McColorHelper.BOLD + megawallsStats.get("finalKills") + McColorHelper.RESET + McColorHelper.GOLD +
+                " | Final Deaths: " + McColorHelper.BOLD + megawallsStats.get("finalDeaths") + McColorHelper.RESET + McColorHelper.GOLD +
+                " | Final K/D " + McColorHelper.BOLD + StatsCommand.calculateKD(megawallsStats.get("finalKills").asInt, megawallsStats.get("finalDeaths").asInt))
+        outText.append("\n")
+        outText.append("\n")
+
+        // Wins.
         outText.append(McColorHelper.GOLD + "Wins: " + McColorHelper.BOLD + megawallsStats.get("wins") + McColorHelper.RESET + McColorHelper.GOLD +
                 " | W/L: " + McColorHelper.BOLD + StatsCommand.calculateKD(megawallsStats.get("wins").asInt, megawallsStats.get("losses").asInt))
         outText.append("\n")
         outText.append("\n")
+
         // Classes
         outText.append(GOLD + "Classes:")
         outText.append("\n")
@@ -49,8 +61,8 @@ object MegaWallsStats {
         outText.append("\n")
         for (mwClass in playerHasClasses) {
             var finalKills = "0"
-            if (megawallsStats.has("finalKills_" + mwClass.capitalize()))
-                finalKills = megawallsStats.get("finalKills_" + mwClass.capitalize()).asString
+            if (megawallsStats.has(mwClass + "_final_kills"))
+                finalKills = megawallsStats.get(mwClass + "_final_kills").asString
             var wins = "0"
             if (megawallsStats.has("wins_" + mwClass.capitalize()))
                 wins = megawallsStats.get("wins_" + mwClass.capitalize()).asString
